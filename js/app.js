50848c744b7141befab17c88368ecdd3219761d3
Bpost129 <Bpost129@yahoo.com>
2025-11-17 11:34:32 -0500

    create array.map example

--- js/app.js
+++ js/app.js
@@ -4,3 +4,10 @@ const friends = ['Melissa', 'Marc', 'Andrew', 'Nick']
 friends.forEach(friend => {
   console.log(`I have a friend named ${friend}`)
 })
+
+
+// map
+const nums = [1, 2, 3]
+const squared = nums.map(num => num * num)
+
+console.log(squared)
